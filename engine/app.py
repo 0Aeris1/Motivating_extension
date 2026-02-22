4b1ff902b813d6657aa28668851237be74d1520c
Aeris <nickkoluko@gmail.com>
2026-02-22 17:39:15 +0100

    Added OpenAI API

--- engine/app.py
+++ engine/app.py
@@ -1,15 +1,43 @@
+import os
+
+from openai import OpenAI
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+class AIClient:
+
+    def __init__(self):
+        self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    def generate(self, prompt: str) -> str:
+    # Calling OpenAI API
+        response = self.client.responses.create(
+            model="gpt-5.2",
+            instructions="You are the happiest motivational assistant",
+            input=prompt,
+            max_output_tokens=150
+        )
+        return response.output_text.strip()
+
+ai_client = AIClient()
+
 class MotivationRequest(BaseModel):
     text: str
 
-
 class MotivationResponse(BaseModel):
     response: str
+
+def generate_motivation(user_text: str) -> str:
+  
+    prompt = (
+            "You are my best motivator.\n"
+            f"User: {user_text}\n"
+            )
+
+    return ai_client.generate(prompt)
 
 app.add_middleware(
         CORSMiddleware,
@@ -22,18 +50,7 @@ app.add_middleware(
 
 @app.post("/motivate", response_model=MotivationResponse)
 def motivate(req: MotivationRequest):
-    user_text = req.text.strip()
-
-    #Fake logic for now
-    if "clean" in user_text.lower():
-        reply = (
-                 "No you're not. That's avoidance.\nOpen the document and write one ugly paragraph.\n20 minutes. Start now."
-                )
-
-    else:
-        reply = (
-                 "You've got this.\nStart with the smallest real step.\n20 minutes. Go."
-                )
+    reply = generate_motivation(req.text)
 
     return {"response": reply}
 
